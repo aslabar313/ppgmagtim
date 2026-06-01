f0f6baff40ad97a83ed0627ee4f3723dfdd3b317
satria <satriaabagus313@gmail.com>
2026-06-01 08:12:13 +0700

    feat: setup complete

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  nitro: true,
   tanstackStart: {
-    server: { entry: "server" },
+    server: { entry: "src/server.ts" },
   },
 });
